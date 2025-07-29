08268b75295ceae5d9254a60806d9b9c6e265c30
Aditya Poonia <adityapoonia.personal@gmail.com>
2025-07-29 06:32:19 +0000

    i have added thwe api key just cross check or not generate

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,10 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
+import 'dotenv/config';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({
+    apiKey: process.env.GEMINI_API_KEY
+  })],
   model: 'googleai/gemini-pro',
 });
