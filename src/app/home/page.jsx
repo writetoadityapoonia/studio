82907ab56ec173e3d4f33a51d73f8c0d7676aa60
Aditya Poonia <adityapoonia.personal@gmail.com>
2025-08-02 17:32:23 +0000

    the location search shuyd be limited to whole karnatka

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -39,7 +39,10 @@ export default function HomePage() {
              <Autocomplete
               apiKey={GOOGLE_MAPS_API_KEY}
               onPlaceSelected={(place) => setPlace(place)}
-              options={{ types: ["geocode"] }}
+              options={{ 
+                types: ["geocode"],
+                componentRestrictions: { country: "in" },
+              }}
               className={cn(
                 "flex h-12 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
               )}
